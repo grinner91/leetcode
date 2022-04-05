9ba4f560d64c84ca36a9aaa542aafd6d50638028
grinner91 <grinner91@gmail.com>
2022-04-05 22:05:28 +0600

    Create remove-duplicates-from-sorted-array.java
    add solution using java

---
+++ Batch4ProblemSolving/remove-duplicates-from-sorted-array.java
@@ -0,0 +1,19 @@
+//https://leetcode.com/problems/remove-duplicates-from-sorted-array/
+
+class Solution {
+    public int removeDuplicates(int[] nums) {
+        if(nums == null || nums.length == 0) {
+            return 0;
+        }
+        
+        int count = 0;
+        for(int i = 1; i < nums.length; i++) {
+            if(nums[count] != nums[i]) {
+                count++;
+                nums[count] = nums[i];
+            }
+        }
+        
+        return count+1;
+    }
+}
