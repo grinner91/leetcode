6aa8ef353c70e77f58deefdf6890d1f3011c25b5
grinner91 <grinner91@gmail.com>
2022-04-16 15:06:44 +0600

    Create fibonacci-number_1_iterative.java

---
+++ Batch4ProblemSolving/fibonacci-number_1_iterative.java
@@ -0,0 +1,16 @@
+//https://leetcode.com/problems/fibonacci-number
+class Solution {
+    public int fib(int n) {
+       if(n <= 1) {
+           return n;
+       } 
+        
+       int[] cache = new int[n + 1];
+        cache[1] = 1;
+        for(int i = 2; i <= n; i++) {
+            cache[i] = cache[i -1] + cache[i - 2];
+        }
+        
+       return cache[n];
+    }
+}
