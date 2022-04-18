eed36697a2b1ee8e7f9ba2b844b1149b044e7463
grinner91 <grinner91@gmail.com>
2022-04-18 10:58:18 +0600

    Update find-first-and-last-position-of-element-in-sorted-array.java

--- Batch4ProblemSolving/find-first-and-last-position-of-element-in-sorted-array.java
+++ Batch4ProblemSolving/find-first-and-last-position-of-element-in-sorted-array.java
@@ -1,34 +1,54 @@
 //https://leetcode.com/problems/find-first-and-last-position-of-element-in-sorted-array
-
 class Solution {
     public int[] searchRange(int[] nums, int target) {
+        //step: handle edge cases 
+        if(nums == null || nums.length == 0) {
+            return new int[] { -1, -1};
+        }
+        //step: find target by binary search and check fist position in left-subarray 
+        int first = findFirstPositionByBinarySearch(nums, target);
+        //step: find traget by binary search and check last position in right sub-array 
         
-        int start = findPositionByBinarySearch(nums, target, true);
-        int end = findPositionByBinarySearch(nums, target,  false);
+        int last = findLastPositionByBinarySearch(nums, target);
         
-        return new int[] {start, end};
+        return new int[] {first, last};
     }
     
-    private int findPositionByBinarySearch(int[] nums, int target,  boolean isStart) {
-        int left = 0; 
+    
+    int findFirstPositionByBinarySearch(int[] nums, int target) {
+        int left = 0;
         int right = nums.length - 1;
         int mid;
         
         while(left <= right) {
             mid = left + (right - left) / 2;
             if(nums[mid] == target) {
-                if(isStart == true) { //starting position
-                    if(mid == left || nums[mid - 1] != target) 
-                        return mid;
-                     else 
-                         right = mid - 1;
-                } else { //for ending position 
-                    if(mid == right || nums[mid+1] != target)
-                        return mid;
-                    else 
-                         left = mid + 1;
-                
-                    }
+                if(mid == left || nums[mid] != nums[mid - 1]) {
+                    return mid;
+                }
+                right = mid - 1;
+            } else if(nums[mid] < target) {
+                left = mid + 1;
+            } else {
+                right = mid - 1;
+            }
+        }
+        
+        return -1;
+    }
+    
+    int findLastPositionByBinarySearch(int[] nums, int target) {
+        int left = 0; 
+        int right = nums.length - 1; 
+        int mid;
+        
+        while(left <= right) {
+            mid = left + (right - left) / 2;
+            if(nums[mid] == target) {
+                if(mid == right || nums[mid] != nums[mid + 1]) {
+                    return mid;
+                }
+                left = mid + 1;
             } else if(nums[mid] < target) {
                 left = mid + 1;
             } else {
