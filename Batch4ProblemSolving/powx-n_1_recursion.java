b1d2f43607a42c8b678c0f0ef81e649a5f4cff0d
grinner91 <grinner91@gmail.com>
2022-04-25 13:32:09 +0600

    Create powx-n_1_recursion.java

---
+++ Batch4ProblemSolving/powx-n_1_recursion.java
@@ -0,0 +1,30 @@
+//50. Pow(x, n)
+//https://leetcode.com/problems/powx-n
+class Solution {
+    public double myPow(double x, int n) {
+        if(n < 0) {
+            x = 1/x;
+            n = -n;
+        }
+        
+        return fastPower(x, n);
+    }
+    
+    private double fastPower(double x, int n) {
+        if(n == 0)  {
+            return 1.0;
+        }
+        if(n == 1) {
+            return x;
+        }
+        
+        double halfPow = fastPower(x, n/2);
+        //if n is even, then x^n = x^n/2 * x^n/2 
+        if(n % 2 == 0) {
+            return halfPow * halfPow;
+        } else {
+            //if n is odd, then  x^n = x^n/2 * x^n/2 * x
+            return halfPow * halfPow * x;
+        }
+    }
+}
