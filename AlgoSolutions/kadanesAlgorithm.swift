a59db13983bb051880d054d390c4ae2cab590594
Mohammad Moniruzzaman <grinner91@gmail.com>
2023-05-29 13:25:48 -0500

    Update kadanesAlgorithm.swift

--- AlgoSolutions/kadanesAlgorithm.swift
+++ AlgoSolutions/kadanesAlgorithm.swift
@@ -1,16 +1,18 @@
 //https://www.algoexpert.io/questions/kadane's-algorithm
 
-class Program 
+class Program {
 //time O(n), space O(1)
+ 
  func kadanesAlgorithm(array: [Int]) -> Int {
       if array.count == 0 {return -1}
       
       var maxSum = array.first!
-      var currentMax = array.first!
+      var currentSum = array.first!
       for i in 1..<array.count {
-           currentMax = max(array[i], array[i] + currentMax)
-           maxSum = max(maxSum, currentMax)
+          currentSum = max(array[i], array[i] + currentSum)
+           maxSum = max(maxSum, currentSum)
       }
     return maxSum
   }
+ 
 }
